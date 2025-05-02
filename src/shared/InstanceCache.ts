d9378193fbe5b3ee11658a29df987883fb9549eb
lun-lun-lun-lun <devlunlun@gmail.com>
2025-05-02 11:29:52 -0600

    Added return and started checks for autocache
    still need to rename lol

--- src/shared/InstanceCache.ts
+++ src/shared/InstanceCache.ts
@@ -9,7 +9,7 @@ type NewVector = ReturnType<typeof vector.create>;
 type Cacheable = Part | Model;
 const FAR = 9999;
 const DEFAULT_CACHE_POSITION = new CFrame(0, FAR, 0);
-const allCaches: Instance[] = [];
+const allCaches: InstanceCache[] = [];
 
 //const MAX_CACHED_INSTANCES = 9000;
 
@@ -19,6 +19,7 @@ export class InstanceCache {
   public hiddenCframe: CFrame = DEFAULT_CACHE_POSITION;
   public cframeTable: CFrame[] = [] as unknown as CFrame[];
   public maximum: number = 9000;
+  public usedThisFrame: boolean = false; //eventually will be changed for a more robust analysis
   constructor(
     template: Cacheable,
     amount: number,
@@ -33,7 +34,7 @@ export class InstanceCache {
     //make a table with the cframe repeated over and over for use with BulkMoveTo
     this.cframeTable = table.create(amount, this.hiddenCframe);
     for (let i = 0; i < amount; i++) {
-      this._addCopy(template.Clone());
+      this._addClone(template);
     }
     //becomes faster than manually moving each model after ~50 moves
     Workspace.BulkMoveTo(
@@ -44,7 +45,8 @@ export class InstanceCache {
   }
 
   //i use the underscore to say that you shouldnt be using the function, only me
-  _addCopy(item: Cacheable) {
+  //will not copy the item by itself.
+  _addItem(item: Cacheable) {
     if (item.IsA('Part')) {
       item.Anchored = true;
     } else {
@@ -61,20 +63,72 @@ export class InstanceCache {
     this.cache.push(item);
   }
 
+  _addClone(originalItem: Cacheable) {
+    const item = originalItem.Clone();
+    if (item.IsA('Part')) {
+      item.Anchored = true;
+    } else {
+      for (const subItem of item.GetDescendants()) {
+        if (
+          subItem.IsA('Part') === true &&
+          subItem.Anchored === false
+        ) {
+          subItem.Anchored = true;
+        }
+      }
+    }
+    item.Parent = Workspace;
+    this.cache.push(item);
+  }
+  add(amount: number | undefined) {
+    this.usedThisFrame = true;
+    const repeats = amount === undefined ? 1 : amount;
+    for (let i = 0; i < repeats; i++) {
+      this._addClone(this.template);
+    }
+  }
+
   get() {
-    print(this.cache.size());
+    this.usedThisFrame = true;
     this.cframeTable.remove(0);
     return this.cache.remove(0);
   }
 
-  return() {
-    task.defer(function () {});
-    // Spawn(function () {
-    //   print('hiiii');
-    // });
+  return(item: Cacheable) {
+    this.usedThisFrame = true;
+    const template = this.template;
+    //fire destroy or something? it wasnt really destroyed though... not sure
+    //for now i'll ignore things like that since they are irrelevant to my usecase, mostly
+    item.Parent = template.Parent;
+    //set miscellaneous attributes
+    for (const attribute of template.GetAttributes()) {
+      //need to skip GUID, they should be unique to each instance (will add GUIDs later btw)
+      const [name, value] = [attribute[0], attribute[1]];
+      if (name === 'UniqueId') {
+        continue;
+      }
+      item.SetAttribute(name, value);
+    }
+    if (item.IsA('Part') && template.IsA('Part')) {
+      item.Color = template.Color;
+      item.Shape = template.Shape;
+      item.Size = template.Size; //not sure if this works, vectors are weird in rbx
+      this._addItem(item);
+    } else if (item.IsA('Model') && template.IsA('Model')) {
+      //item.ScaleTo(template.GetScale());
+      //would theoretically have to handle getting back removed children too?
+      //sounds like too much work for a part of it im not really using
+      item.Destroy();
+      this._addClone(template);
+    }
   }
 }
 
-RunService.Heartbeat.Connect(function () {});
+RunService.Heartbeat.Connect(function (deltaTime) {
+  for (const cache of allCaches) {
+    //print(cache);
+    cache.usedThisFrame = false;
+  }
+});
 
 //export default {};
